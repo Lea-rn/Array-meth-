a11ccd0ff5d088b9df6fa9e6f1a991505970d00d
achrefmechergui <achreffcb123achref@gamil.com>
2025-02-08 22:01:45 +0100

    array-meth

---
+++ app.js
@@ -0,0 +1,65 @@
+////////////// slice //////////////// (copie)
+/// matbadelech l origin array .. 
+/// matehssebech el end number ...
+
+// let arr = ["a","b","c","d","e"]
+
+// syntaxe :::  Array.slice(Start number,end number)
+// console.log(arr.slice(1,4))
+// console.log(arr)
+// console.log(arr.slice(1))
+
+// console.log(arr[arr.length-1])
+// console.log(arr.slice(-1))
+//********************************* */
+
+//// spread operator : 
+// console.log(...arr)
+
+////////////// splice //////////////////
+//// tbadel el origin array 
+/// ==> taamel delete lel partie ... 
+
+// let arr = ["a","b","c","d","e"]
+// console.log("before:" , arr)
+
+// arr.splice(1,3)
+// arr.splice(2,2)
+/// syntaxe : splice("mnin bech tabda tfasakh" , "kadeh bech tfasakh")
+
+// console.log("after:" , arr)
+
+//////// reverse ///   ( tbadel el origin array)
+
+// let arr = ["a","b","c","d","e"] ; 
+// console.log(arr)
+// arr.reverse() ; 
+// console.log(arr)
+
+///// concat ////// 
+// let arr = ["a","b","c","d","e"]
+// const arr2 = ["f" ,"j" , "h"]
+
+// const arr3 = arr.concat(arr2)
+// console.log(arr3)
+// const arr3 = [...arr , ...arr2]
+// console.log(arr3)
+
+
+//// join  (copie) ::: 
+
+// let arr = ["a","b","c","d","e"]
+// console.log(arr)
+// console.log(arr.join("!!"))
+// console.log(arr)
+
+
+
+//// push() ; pop () ; shift() ; unshif() ; includes() ; indexOf ; length ;slice ;
+//  splice ; reverse () ; concat () ; join ()
+
+
+
+
+
+
